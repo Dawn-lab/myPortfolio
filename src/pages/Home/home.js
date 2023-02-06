f4e66e4938985cf103214f5bd8816eff990e1e81
Dawn-lab <julius.githinji@outlook.com>
2023-02-06 14:17:50 +0300

    Image importation animation on the home page

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,5 +1,7 @@
-import { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./home.scss";
+import foto from "./../../IMG_20220331_123215-removebg-preview.png";
+import splash from "./../../luca-nicoletti-O8CHmj0zgAg-unsplash-removebg-preview.png";
 
 function App() {
     const [mouseCoordinates, setMouseCoordinates] = useState({ x: 0, y: 0 });
@@ -39,29 +41,51 @@ function App() {
     }, []);
 
     return (
-        <div className="app">
-            <h2>This is the home page</h2>
-            <div className="eyebrow_container">
-                <div ref={eyeBrowLeft} className="eye_brow left"></div>
-                <div ref={eyeBrowRight} className="eye_brow right"></div>
+        <React.Fragment>
+            <div className="home-page">
+                <div className="image-wrapper">
+                    <img className="image" src={foto} alt=""></img>
+                    <img className="splash" src={splash} alt=""></img>
+                </div>
+                <div className="app">
+                    {/* <h2>This is the home page</h2> */}
+                    <div className="intro">
+                        <h1 className="name">IRUNGU G.</h1>
+                        <h3>Hello_</h3>
+                        <h2>I'm a Software Developer</h2>
+                        <p>
+                            A full Stack developer with incredible skills in creating successful websites that meet customer needs. I am proficient enough in NodeJS, ReactJs, MongoDB, TypeScript/JavaScript; well-versed in design standards and user preferences.
+                        </p>
+                        <div className="btns">
+                            <button className="btn">Download Resume</button>
+                            <button className="btn">Hire me</button>
+                        </div>
+                    </div>
+                    <div className="images apps brows">
+                        <div className="eyebrow_container">
+                            <div ref={eyeBrowLeft} className="eye_brow left"></div>
+                            <div ref={eyeBrowRight} className="eye_brow right"></div>
+                        </div>
+                        <div className="eye_container">
+                            <div
+                                ref={eyeLeft}
+                                style={{
+                                    transform: `rotate(${calcAngle(eyeLeft)}deg)`,
+                                }}
+                                className="eye"
+                            ></div>
+                            <div
+                                ref={eyeRight}
+                                style={{
+                                    transform: `rotate(${calcAngle(eyeRight)}deg)`,
+                                }}
+                                className="eye"
+                            ></div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="eye_container">
-                <div
-                    ref={eyeLeft}
-                    style={{
-                        transform: `rotate(${calcAngle(eyeLeft)}deg)`,
-                    }}
-                    className="eye"
-                ></div>
-                <div
-                    ref={eyeRight}
-                    style={{
-                        transform: `rotate(${calcAngle(eyeRight)}deg)`,
-                    }}
-                    className="eye"
-                ></div>
-            </div>
-        </div>
+        </React.Fragment>
     );
 }
 
--- src/pages/Layout/layout.js
+++ src/pages/Layout/layout.js
@@ -1,6 +1,7 @@
 import { Outlet, Link } from "react-router-dom";
 import React from "react";
 import "./layout.scss";
+import "./../Home/home.scss"
 
 
 const Layout = () => {
@@ -10,13 +11,13 @@ const Layout = () => {
                 <Link to="/" className="logo"><img alt="logo" src=""></img></Link>
                 <ul>
                     <li >
-                        <Link to="/" className="navlinks">Home</Link>
+                        <Link to="/" className=" btn navlinks">Home</Link>
                     </li>
                     <li >
-                        <Link to="/contacts" className="navlinks">Contact</Link>
+                        <Link to="/contacts" className=" btn navlinks">Contact</Link>
                     </li>
                     <li>
-                        <Link to="/projects" className="navlinks">Projects</Link>
+                        <Link to="/projects" className=" btn navlinks">Projects</Link>
                     </li>
                 </ul>
             </nav>
