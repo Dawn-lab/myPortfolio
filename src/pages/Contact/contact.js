f16f7f8aba327074c574c836e64e46542cf7aaad
Dawn-lab <julius.githinji@outlook.com>
2023-02-17 23:05:15 +0300

    Modified the project's page

--- src/pages/Contact/contact.js
+++ src/pages/Contact/contact.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { useState } from "react";
-import "./contact.scss"
+import "./contact.scss";
+import "./../Home/home.scss"
 
 function Contact() {
 
 
 
     const[name, setName] = useState('');
-
     const[companyName, setCompanyName] = useState('');
+    const[emailAddress, setEmailAddress] = useState('');
+    const[phoneNumber, setPhoneNumber] = useState('');
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -19,11 +21,11 @@ function Contact() {
     return (
         <>
             <article>
-                <h1> Contact form </h1>{" "}
+                <h1> Contact form </h1>
                 <h2>
                     Please complete the form below so we can forward your contact request
                     to Julius Githinji:
-                </h2>{" "}
+                </h2>
                 <form
                     id="my-form"
                     action="https://formspree.io/f/mnqwbple"
@@ -31,11 +33,11 @@ function Contact() {
                     onSubmit={handleSubmit}
                 >
                     <div className="container">
-                        <h1> Hi Julius, </h1>{" "}
+                        <h1> Hi Julius, </h1>
                         <div className="info">
-                            <div> I am </div>{" "}
+                            <div> I am </div>
                             <div className="input-box">
-                                <div className="small"> First Name and Last Name * </div>{" "}
+                                <div className="small"> First Name and Last Name * </div>
                                 <input
                                     maxlength="96"
                                     placeholder="First Name Last Name"
@@ -45,10 +47,10 @@ function Contact() {
                                     required
                                     onChange={(e) => setName(e.target.value)}
                                 />
-                            </div>{" "}
-                            <div> from </div>{" "}
+                            </div>
+                            <div> from </div>
                             <div className="input-box">
-                                <div className="small"> Company Name </div>{" "}
+                                <div className="small"> Company Name </div>
                                 <input
                                     maxlength="100"
                                     placeholder="Company Name"
@@ -57,24 +59,24 @@ function Contact() {
                                     name="companyName"
                                     onChange={(e) => setCompanyName(e.target.value)}
                                 />
-                            </div>{" "}
-                        </div>{" "}
-                        <div> and I’ m contacting you to *: </div>{" "}
+                            </div>
+                        </div>
+                        <div> and I’ m contacting you to *: </div>
                         <div className="smaller">
-                            Please state reason(s) why you 're reaching out:{" "}
-                        </div>{" "}
+                            Please state reason(s) why you 're reaching out:
+                        </div>
                         <div>
                             <legend>
-                                <label for="message"> Your Message goes below </label>{" "}
-                                {/* <br> */}{" "}
+                                <label for="message"> Your Message goes below </label>
+                                {/* <br> */}
                                 <textarea
                                     className="message"
                                     name="message"
                                     id="someText"
                                     required
-                                ></textarea>{" "}
-                            </legend>{" "}
-                        </div>{" "}
+                                ></textarea>
+                            </legend>
+                        </div>
                         <div className="complete">
                             <div className="contacts">
                                 <div className="input-box">
@@ -83,56 +85,58 @@ function Contact() {
                                         <div className="why" id="hidden">
                                             We ask for your contact info to enable communication
                                             between You and Julius.We will never share it with 3 rd
-                                            party sources.{" "}
-                                        </div>{" "}
-                                    </div>{" "}
-                                    <div className="small"> Email address * </div>{" "}
+                                            party sources.
+                                        </div>
+                                    </div>
+                                    <div className="small"> Email address * </div>
                                     <input
                                         placeholder="Email address"
                                         type="text"
-                                        value=""
+                                        value={emailAddress}
                                         name="emailAddress"
                                         required
+                                        onChange={(e) => setEmailAddress(e.target.value)}
                                     />
-                                </div>{" "}
+                                </div>
                                 <div className="input-box">
-                                    <div className="small"> Phone number </div>{" "}
+                                    <div className="small"> Phone number </div>
                                     <input
                                         maxlength="20"
                                         placeholder="Phone number"
                                         type="text"
-                                        value=""
+                                        value={phoneNumber}
                                         name="number"
+                                        onChange={(e) => setPhoneNumber(e.target.value)}
                                     />
-                                </div>{" "}
-                            </div>{" "}
+                                </div>
+                            </div>
                             <div className="right">
-                                <div> Looking forward to hearing from you! </div>{" "}
-                                <div className="fullName"> First Name Last Name </div>{" "}
-                                <button className="btn" type="submit">
-                                    {" "}
-                                    SEND→{" "}
-                                </button>{" "}
-                                <div id="status"> </div>{" "}
-                            </div>{" "}
-                        </div>{" "}
-                    </div>{" "}
-                </form>{" "}
-            </article>{" "}
+                                <div> Looking forward to hearing from you! </div>
+                                <div className="fullName"> First Name Last Name </div>
+                                <button className="btn submit" type="submit">
+                                    
+                                    Submit
+                                </button>
+                                <div id="status"> </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </article>
             <footer>
                 <div className="socials">
-                    <span className="invert git">
-                        <img src="./assets/svg/github.svg" alt="" />{" "}
-                    </span>{" "}
+                    <span className="git">
+                        <img src="./assets/svg/github.svg" alt="" />
+                    </span>
                     <span className="twitter">
-                        <img src="./assets/svg/twitter-color.svg" alt="" />{" "}
-                    </span>{" "}
+                        <img src="./assets/svg/twitter-color.svg" alt="" />
+                    </span>
                     <span className="linkedin">
-                        <img src="./assets/svg/linkedin-square-color.svg" alt="" />{" "}
-                    </span>{" "}
-                </div>{" "}
-                & copy; Julius Githinji• 2022{" "}
-            </footer>{" "}
+                        <img src="./assets/svg/linkedin-square-color.svg" alt="" />
+                    </span>
+                </div>
+                & copy; Julius Githinji• 2023
+            </footer>
         </>
     );
 }
